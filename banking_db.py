2ebca2619a660965a2e0f1ec0114a473aa373cda
AJorangeBlue <kingaiden17@gmail.com>
2026-04-27 14:06:30 -0500

    UI Options update

--- banking_db.py
+++ banking_db.py
@@ -14,3 +14,20 @@ def bucket_Bank:
     balance REAL
     )
   ''')
+
+  connect.commit()
+  connect.close()
+
+  print("  === Bucket Bank ===")
+  print("=======================")
+  print("| 1. Create Account  |")
+  print("| 2. Deposit Money   |")
+  print("| 3. Withdraw Money  |")
+  print("| 4. Check Balance   |")
+  print("| 5. Exit Bank       |")
+  print("=======================")
+  print(" ====================")
+
+  choices = input("Enter your options (1-5): ")
+
+
